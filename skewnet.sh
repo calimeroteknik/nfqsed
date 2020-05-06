9fa51d824fededc36a453d554749c23d356b4048
CTK <calimeroteknik@free.fr>
2020-05-06 22:35:46 +0200

    Turnkey prank

---
+++ skewnet.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+iptables_rule="FORWARD -p tcp -j NFQUEUE --queue-num 0"
+iptables_cleanup="iptables -D $iptables_rule"
+
+trap "$iptables_cleanup" EXIT || exit 1
+
+set -x
+
+while $iptables_cleanup 2>/dev/null; do :; done
+
+iptables -A $iptables_rule || exit 1
+
+./nfqsed -vv \
+ -s '/accept-encoding:/X-Pwnd: encoding' \
+ -s '/font-family:?????????????/transform:rotate(179deg);'
