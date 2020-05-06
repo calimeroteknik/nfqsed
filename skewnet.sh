e63c8a3640843ec9650f2259ea06797f94b8194d
CTK <calimeroteknik@free.fr>
2020-05-06 23:20:55 +0200

    Note about iptables chains

--- skewnet.sh
+++ skewnet.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-iptables_rule="FORWARD -p tcp -j NFQUEUE --queue-num 0"
-iptables_cleanup="iptables -D $iptables_rule"
+# use INPUT to see effects locally
+# use FORWARD to affect only packets routed for others
+iptables_rule="FORWARD -p tcp -j NFQUEUE --queue-num 0
 
+iptables_cleanup="iptables -D $iptables_rule"
 trap "$iptables_cleanup" EXIT || exit 1
 
 set -x
