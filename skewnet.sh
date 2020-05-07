389597d858c9bb24ee710df8a2c1853d7280a290
CTK <calimeroteknik@free.fr>
2020-05-07 17:44:48 +0200

    Fix quote

--- skewnet.sh
+++ skewnet.sh
@@ -2,7 +2,7 @@
 
 # use INPUT to see effects locally
 # use FORWARD to affect only packets routed for others
-iptables_rule="FORWARD -p tcp -j NFQUEUE --queue-num 0
+iptables_rule="FORWARD -p tcp -j NFQUEUE --queue-num 0"
 
 iptables_cleanup="iptables -D $iptables_rule"
 trap "$iptables_cleanup" EXIT || exit 1
